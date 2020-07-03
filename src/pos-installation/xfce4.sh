c1fd6304282c49550ea7ea1a20a0d0defd2821cd
Lucas-Trombetta <65026007+Com-Pensa@users.noreply.github.com>
2020-07-03 14:37:06 +0000

    Create xfce4.sh

---
+++ src/pos-installation/xfce4.sh
@@ -0,0 +1,6 @@
+pacman -S xorg-server
+pacman -S xf86-video-intel libgl mesa
+pacman -S xfce4 xfce4-goodies tilix chromium pulseaudio pavucontrol
+pacman -S lxdm
+systemctl enable lxdm.enable
+pacman -S git code

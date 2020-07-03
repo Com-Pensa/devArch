62fd41c846cada30047e956d50075255784a4fee
Lucas-Trombetta <65026007+Com-Pensa@users.noreply.github.com>
2020-07-03 14:51:07 +0000

    Update xfce4.sh

--- src/pos-installation/xfce4.sh
+++ src/pos-installation/xfce4.sh
@@ -2,5 +2,5 @@ pacman -S xorg-server
 pacman -S xf86-video-intel libgl mesa
 pacman -S xfce4 xfce4-goodies tilix chromium pulseaudio pavucontrol
 pacman -S lxdm
-systemctl enable lxdm.enable
+systemctl enable lxdm.service
 pacman -S git code

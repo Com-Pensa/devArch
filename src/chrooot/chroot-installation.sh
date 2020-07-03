4ada23659ba6b5d3551153e56becf94bf75d7209
Lucas-Trombetta <65026007+Com-Pensa@users.noreply.github.com>
2020-07-03 14:28:40 +0000

    Create chroot-installation.sh

---
+++ src/chrooot/chroot-installation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Time
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+hwclock --systohc
+
+# Some configurations
+echo "en-US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
+
+echo "arch" > /etc/hostname
+
+echo "127.0.0.1            localhost" >> /etc/hosts
+echo "::1                  localhost" >> /etc/hosts
+echo "127.0.0.1 myhostname.localdomain myhostname" >> /etc/hosts
+
+# Passwords and new user
+passwd "123123"
+useradd -m -g users -G wheel -s /bin/bash user
+passwd user "123123"
+
+echo " + A senha e 123123"
+
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
+# General packages
+pacman -S networkmanager network-manager-applet dosfstools wireless-tools dialog grub
+
+# Grub install
+grub-install --target=i386-pc --recheck /dev/sda
+cp /usr/share/locale/en@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+exit

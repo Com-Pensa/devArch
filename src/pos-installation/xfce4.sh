bcb913e71f2f39070156ff0391376b46376fe967
Lucas-Trombetta <65026007+Com-Pensa@users.noreply.github.com>
2020-07-05 19:14:14 -0300

    Update xfce4.sh

--- src/pos-installation/xfce4.sh
+++ src/pos-installation/xfce4.sh
@@ -1,6 +1,28 @@
-pacman -S xorg-server
-pacman -S xf86-video-intel libgl mesa
-pacman -S xfce4 xfce4-goodies tilix chromium pulseaudio pavucontrol
-pacman -S lxdm
-systemctl enable lxdm.service
-pacman -S git code
+# General
+pacman -S xorg --noconfirm
+pacman -S xf86-video-intel xf86-input-synaptics xf86-input-libinput libgl mesa --noconfirm
+
+# XFCE4 and LightDM
+pacman -S xfce4 xfce4-goodies --noconfirm
+pacman -S lightdm lightdm-gtk-greeter --noconfirm
+
+pacman -S tilix git code --noconfirm
+pacman -S chromium --noconfirm
+
+# Audio
+pacman -S alsa-utils pulseaudio paprefs pavucontrol --noconfirm
+
+# Setting the keyboard
+localectl set-x11-keymap br abnt2
+
+systemctl enable lightdm
+
+# The background
+rm /usr/share/backgrounds/xfce/xfce-stripes.png
+
+pacman -S wget --noconfirm
+wget https://i.imgur.com/xLc7V7B.jpg
+cp xLc7V7B.jpg /usr/share/backgrounds/xfce/xfce-stripes.jpg
+
+rm xLc7V7B.jpg
+pacman -R wget
